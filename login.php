5706c60a926bd4dd1c77238f6df10853f220189c
Marco <markosayajin@hotmail.com>
2022-11-06 12:27:42 -0600

    changes

--- login.php
+++ login.php
@@ -10,6 +10,7 @@ class Controller {
     }
 
     public function login(){
+        echo 'hola';
         $_POST = json_decode(file_get_contents('php://input'), true);
         if(isset($_POST['username']) && isset($_POST['password']) ){
             $username = $_POST['username'];
